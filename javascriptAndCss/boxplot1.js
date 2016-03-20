fd26c3615a1e30916fea030f4559c182e0115f19
Tom_Laptop <tom.gebhardt@uni-rostock.de>
2016-03-20 11:38:52 +0100

    delete startinfo

--- javascriptAndCss/boxplot1.js
+++ javascriptAndCss/boxplot1.js
@@ -3,6 +3,7 @@ function boxplot(date1, date2){
 	d3.selectAll('#charts').selectAll('svg').remove();
 	d3.selectAll('.onoffswitch').remove();
 	d3.selectAll('#info > *').remove();
+	d3.selectAll('#charts > *').remove();
 	$('#box1Button').fadeIn();
 
 // Variables for Boxplots
