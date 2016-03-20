a1bc387cc1d23aa9020233cc7929fda7297d9d79
Martin Scharm <martin@binfalse.de>
2016-03-20 16:12:10 +0100

    got rid of the ?-urls, using base64 now

--- resources/index.php
+++ resources/index.php
@@ -11,28 +11,47 @@ function strip_tailing_slashes ($str)
     return $str;
 }
 
+function prep ($str)
+{
+    return strip_tailing_slashes (base64_decode ($str));
+}
 
-	$userAgent = $_SERVER['HTTP_USER_AGENT'];
-	$model = strip_tailing_slashes ($_GET['model']);
-	$version = strip_tailing_slashes ($_GET['version']);
-	$repo = strip_tailing_slashes ($_GET['repo']);
-	
-	
-    $isRequestComingFromRealUser = strpos($userAgent, 'Java') === false;
-    $isCellModelRepo = strpos($repo, 'biomodels') == false;
 
-	if( $isRequestComingFromRealUser){
-		if($isCellModelRepo){
-			header("Location: http://$repo/file/$version/$model");
-		}else{
-			header("Location: http://$repo/$model");
-		}
-	}else{
-		// the request is coming from bives
-		if($isCellModelRepo){
-			header("Location: http://$repo/download/$version/$model");
-		}else{
-			header("Location: http://biomodels.lesscomplex.org/releases/$model.xml/$version/$model.xml");
-		}
-	}
+$url = explode ("/", $_SERVER["REQUEST_URI"]);
+if (count ($url) != 5 || $url[1] != "resources")
+    die ("access denied.");
+
+
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+$repo = prep ($url[2]);
+$model = prep ($url[3]);
+$version = prep ($url[4]);
+
+
+$isRequestComingFromRealUser = strpos($userAgent, 'Java') === false;
+$isCellModelRepo = strpos($repo, 'biomodels') === false;
+
+if( $isRequestComingFromRealUser)
+{
+    if($isCellModelRepo)
+    {
+        header("Location: http://$repo/file/$version/$model");
+    }
+    else
+    {
+        header("Location: http://$repo/$model");
+    }
+}
+else
+{
+    // the request is coming from bives
+    if($isCellModelRepo)
+    {
+        header("Location: http://$repo/download/$version/$model");
+    }
+    else
+    {
+        header("Location: http://biomodels.lesscomplex.org/releases/$model.xml/$version/$model.xml");
+    }
+}
 ?>
