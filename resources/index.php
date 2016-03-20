d9b5ffc2df6de5a0e121b945f8baa52301f7b087
Tamas Galli <tamas.galli@gmail.com>
2016-03-20 14:16:59 +0100

    redirection for the links

--- resources/index.php
+++ resources/index.php
@@ -1,13 +1,25 @@
 <?php
+	$userAgent = $_SERVER['HTTP_USER_AGENT'];
+	$model = $_GET['model'];
+	$version = $_GET['version'];
+	$repo = $_GET['repo'];
+	
+	
+	$isRequestComingFromRealUser = strpos($userAgent, 'Java') !== false;
+	$isCellModelRepo = strpos($repo, 'biomodels') !== false;
 
-
-$model = $_GET['model'];
-$version = $_GET['version'];
-
-#header("Location: http://binfalse.de/$model/$version");
-
-
-
- echo $_SERVER['HTTP_USER_AGENT'];
-
+	if( $isRequestComingFromRealUser){
+		if($isCellModelRepo){
+			header("Location: http://$repo/file/$version/$model");
+		}else{
+			header("Location: http://$repo/$model");
+		}
+	}else{
+		// the request is coming from bives
+		if($isCellModelRepo){
+			header("Location: http://$repo/download/$version/$model");
+		}else{
+			header("Location: http://biomodels.lesscomplex.org/releases/$model.xml/$version/$model.xml");
+		}
+	}
 ?>
