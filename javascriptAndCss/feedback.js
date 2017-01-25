0441ade5f87c11db8148076b0642e8eef7c13f51
Martin Scharm <martin@binfalse.de>
2017-01-25 08:26:49 +0100

    changed the default feedback url to point to github

--- javascriptAndCss/feedback.js
+++ javascriptAndCss/feedback.js
@@ -1,3 +1,3 @@
 function giveFeedback(){
-	 window.open("https://sems.uni-rostock.de/trac/stats-website/newticket?from=" + window.location.href)
+	 window.open("https://github.com/SemsProject/MOST/issues/new?body=%0A%0A___%0AI%20came%20from%20the%20MOST%20website%20through%20the%20feedback%20button")
 }
