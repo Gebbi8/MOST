64f8f69563ece1cbc4215f920105c2d5c97a521a
Martin Scharm <martin@binfalse.de>
2017-07-25 17:24:52 +0200

    allow searching for models with substrings
    you can now search for BIOM to get all curated models from biomodels

--- javascriptAndCss/filters.js
+++ javascriptAndCss/filters.js
@@ -216,14 +216,19 @@ function filterTimeDiffs (table)
 }
 
 
+
+
+
 // filter by several model IDs in files table
 function filterIdFiles(table){
 	var filtered = {};
  	for(var id in table){
-		for(var i = 0; i < modelsFilter.length; i++){
- 			if(table.hasOwnProperty(id) && modelsFilter.indexOf(table[id].model) > -1){
-				filtered[id] = table[id];
-			} 
+		if(table.hasOwnProperty(id) {
+			for(var i = 0; i < modelsFilter.length; i++){
+				if(table[id].model.indexOf(modelsFilter) > -1){
+					filtered[id] = table[id];
+				} 
+			}
 		}
 	} 
 	return filtered;
@@ -235,7 +240,7 @@ function filterIdDiffs (table)
 	var filtered = [];
  	for (var id = 0; id < table.length; id++)
 	{
-		if (modelsFilter.indexOf(table[id]["model"]) > -1)
+		if (table[id]["model"].indexOf(modelsFilter) > -1)
 			filtered.push (table[id]);
 	} 
 	return filtered;
